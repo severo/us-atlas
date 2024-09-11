22706d08f1354809f7b0bc67e7e567f3829b8c07
Sylvain Lesage <severo@rednegra.net>
2024-09-12 00:32:58 +0200

    2017 -> 2023

--- properties.js
+++ properties.js
@@ -2,34 +2,45 @@ const shapefile = require("shapefile");
 
 Promise.all([
   parseInput(),
-  shapefile.read("build/cb_2017_us_county_5m.shp", undefined, {encoding: "utf-8"}),
-  shapefile.read("build/cb_2017_us_state_5m.shp", undefined, {encoding: "utf-8"})
+  shapefile.read("build/cb_2023_us_county_5m.shp", undefined, {
+    encoding: "utf-8",
+  }),
+  shapefile.read("build/cb_2023_us_state_5m.shp", undefined, {
+    encoding: "utf-8",
+  }),
 ]).then(output);
 
 function parseInput() {
   return new Promise((resolve, reject) => {
     const chunks = [];
     process.stdin
-        .on("data", chunk => chunks.push(chunk))
-        .on("end", () => {
-          try { resolve(JSON.parse(chunks.join(""))); }
-          catch (error) { reject(error); }
-        })
-        .setEncoding("utf8");
+      .on("data", (chunk) => chunks.push(chunk))
+      .on("end", () => {
+        try {
+          resolve(JSON.parse(chunks.join("")));
+        } catch (error) {
+          reject(error);
+        }
+      })
+      .setEncoding("utf8");
   });
 }
 
 function output([topology, counties, states]) {
-  counties = new Map(counties.features.map(d => [d.properties.GEOID, d.properties]));
-  states = new Map(states.features.map(d => [d.properties.GEOID, d.properties]));
+  counties = new Map(
+    counties.features.map((d) => [d.properties.GEOID, d.properties])
+  );
+  states = new Map(
+    states.features.map((d) => [d.properties.GEOID, d.properties])
+  );
   for (const county of topology.objects.counties.geometries) {
     county.properties = {
-      name: counties.get(county.id).NAME
+      name: counties.get(county.id).NAME,
     };
   }
   for (const state of topology.objects.states.geometries) {
     state.properties = {
-      name: states.get(state.id).NAME
+      name: states.get(state.id).NAME,
     };
   }
   process.stdout.write(JSON.stringify(topology));
